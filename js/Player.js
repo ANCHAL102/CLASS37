d739afeb57dd70e7c13357b348cc30b62215ec5d
ANCHAL102 <80636070+ANCHAL102@users.noreply.github.com>
2021-06-11 21:30:48 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,16 @@
+class Player{
+    constructor(){
+this.name=null;
+    }
+getCount(){
+    database.ref('playerCount').on("value",(data)=>{
+        playerCount=data.val();
+    })
+
+}
+updateCount(count){
+database.ref('/').update({
+    playerCount:count 
+})
+}
+}
